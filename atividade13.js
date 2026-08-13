1affee3a123432cd261aae2225ec6d5dfe4d0af0
Maria Eduarda <maria.e.oliveira25@edu.senai.br>
2026-08-13 11:18:27 -0300

    Par ou impar

---
+++ atividade13.js
@@ -0,0 +1,7 @@
+    function verificarPar(numero) {
+        if (numero % 2 === 0 ) {
+            console.log("Esse número é par ");
+        }else 
+            console.log ( "Esse número é impar");
+    }
+    verificarPar(205);
