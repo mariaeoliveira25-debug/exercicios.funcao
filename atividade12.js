3bef23b33d8722c497381cc91e941b3219779b3d
Maria Eduarda <maria.e.oliveira25@edu.senai.br>
2026-08-13 11:07:25 -0300

    Numero positivo ou negativo

---
+++ atividade12.js
@@ -0,0 +1,11 @@
+function verificarNumero(numero){
+    if (numero > 0){
+        console.log ("Número positivo");
+        }else if (numero < 0){
+            console.log ("Número negativo");
+        }else{
+        console.log ("Número 0");
+    }
+}
+
+verificarNumero(202);
