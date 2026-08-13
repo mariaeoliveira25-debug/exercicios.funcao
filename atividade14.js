d96b9adfe3d7dc46209ef267f2d30efa89ef882e
Maria Eduarda <maria.e.oliveira25@edu.senai.br>
2026-08-13 11:23:32 -0300

    Aprovado ou reprovado

---
+++ atividade14.js
@@ -0,0 +1,9 @@
+    function verificarAprovacao(media) {
+         if (media >= 6 ){
+        return "Aprovado";
+   } else {
+    return "Reprovado";
+   }
+    }
+    console.log(verificarAprovacao(10));
+ 
